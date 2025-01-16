aa4cab06b744bfdd25b35bbd298e0bf3ad593abf
Jona <jona07072000@gmail.com>
2025-01-15 21:17:11 -0500

    FEAT <backend> Cambios para probar producción.

--- Server/modules/Auth/controllers/loginController.js
+++ Server/modules/Auth/controllers/loginController.js
@@ -68,8 +68,8 @@ const loginUsuario = [
             // Establecer la cookie con el token
             res.cookie('access_token', token, {
                 httpOnly: true, // Sólo a través del servidor (No Javascript del cliente).
-                secure: true,
-                sameSite: 'none' // Protección contra CSRF (LAX cuando está en local).
+                secure: false,
+                sameSite: 'lax' // Protección contra CSRF (LAX cuando está en local).
                 // secure: process.env.NODE_ENV === 'production', // Sólo por HTTPS en producción
                 // sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax', // Protección contra CSRF (LAX cuando está en local).
                 // maxAge: 2 * 60 * 60 * 1000 // 2 horas en milisegundos
